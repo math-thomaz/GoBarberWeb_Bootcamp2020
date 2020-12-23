b140d2681e3d0e3323d22cd34550e753753d728b
Matheus Thomaz <matheustthomaz@gmail.com>
2020-12-23 03:34:35 -0300

    Header added on dashboard page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,6 +5,8 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
 import Dashboard from '../pages/Dashboard';
 
@@ -12,7 +14,9 @@ const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/signup" exact component={SignUp} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/forgot-password" component={ForgotPassword} />
+      <Route path="/reset-password" component={ResetPassword} />
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
